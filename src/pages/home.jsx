c847a8dc856942ef9a4017ff9e4734b05203fb63
Robson Nascimento <robsonnascimento@MacBook-Pro-de-Robson.local>
2022-08-05 11:46:32 -0300

    create component userPicture

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,14 @@
 import Container from '../components/container';
 import Header from '../components/header';
 import UserContainer from '../components/userContainer';
+import UserPicture from '../components/userPicture';
 
 export default function Home() {
     return (
         <Container>
             <Header />
             <UserContainer>
-                
+                <UserPicture url="" alternativeText="image"  />
             </UserContainer>
         </Container>
     );
