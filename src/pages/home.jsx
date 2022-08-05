009551f4ea49ccd1b91664049b3a45edb0780ba6
Robson Nascimento <robsonnascimento@MacBook-Pro-de-Robson.local>
2022-08-05 12:18:02 -0300

    create component userNumbers

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,16 +1,31 @@
+import React, { useContext } from 'react';
+import { useQuery } from '../hooks/useQuery';
+
 import Container from '../components/container';
 import Header from '../components/header';
 import UserContainer from '../components/userContainer';
 import UserPicture from '../components/userPicture';
 import UserDetails from '../components/userDetails';
+import UserNumbers from '../components/userNumbers';
+
+import { context } from '../context';
 
 export default function Home() {
+    const ctx = useContext(context);
+    const query = useQuery();
+
     return (
         <Container>
-            <Header />
+            <Header username={query.get('username')} />
+
             <UserContainer>
-                <UserPicture url="" alternativeText="image"  />
-                <UserDetails name="" login=""  bio="" />
+                {ctx.userData?.name ?
+                    <React.Fragment>
+                        <UserPicture url={ctx.userData?.avatar_url} />
+                        <UserDetails name={ctx.userData?.name} username={ctx.userData?.login} bio={ctx.userData?.bio} />
+                        <UserNumbers repos={ctx.userData?.public_repos} followers={ctx.userData?.followers} following={ctx.userData?.following} />
+                    </React.Fragment>
+                : undefined }
             </UserContainer>
         </Container>
     );
